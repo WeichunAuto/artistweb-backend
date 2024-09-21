82e86a23e1e49651407e0c27329edba5e1afe34d
WeichunAuto <seven.wangweichun@outlook.com>
2024-09-21 20:22:13 +1200

    initialize the project.

---
+++ src/main/java/com/bobby/artistweb/ArtistwebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bobby.artistweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistwebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistwebBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bobby/artistweb/ArtistwebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobby.artistweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistwebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
